fbed2c03922ff5a07ebbda52e045fc67cd7dafcb
cmronk <christinemronk@gmail.com>
2018-09-06 13:47:07 -0500

    updated heroku credentials

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -12,7 +12,10 @@ const About = (props) => {
                 {/* <a className="btn btn-light btn" role="button">Learn More</a> */}
 
                 <div className="container">
-                    <p>We believe mentorship matters, but only about 54% of women in the workforce participate. This percentage is even less for women in the tech industry.  Check out what Forbes has to say
+                <h3>Why mentorship?</h3>
+                    <p>Mentors provide insight, support, and promote engagement within a company or career. 
+                    </p>
+                    <p>We believe mentors and mentorship are valuable, but only about half of women in the workforce participate. This percentage is even less for women in the tech industry. Check out what Forbes has to say
                     <a href="https://www.forbes.com/sites/margiewarrell/2017/06/24/women-mentoring/#65a6be9d22db" target="_blank"> here</a>.
                     </p>
                     <p>
