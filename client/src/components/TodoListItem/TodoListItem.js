2b925fae1fbe50208062200fdddc121963da14d0
cmronk <christinemronk@gmail.com>
2018-09-15 19:23:23 -0500

    updated button styling

--- client/src/components/TodoListItem/TodoListItem.js
+++ client/src/components/TodoListItem/TodoListItem.js
@@ -14,14 +14,14 @@ export default class TodosListItem extends React.Component {
             return (
                 <td>
                     <button className="btn btn-dark" onClick={this.editTask.bind(this)}>Save<i className="fa fa-floppy-o" aria-hidden="true"></i></button>
-                    <button className="cancel-btn btn" onClick={this.setEditState.bind(this, false)}>Cancel<i className="fa fa-ban" aria-hidden="true"></i></button>
+                    <button className="btn btn-dark" onClick={this.setEditState.bind(this, false)}>Cancel<i className="fa fa-ban" aria-hidden="true"></i></button>
                 </td>
             );
         }
         return (
             <td>
                 <button className="btn btn-dark" onClick={this.setEditState.bind(this, true)}>Edit<i className="fa fa-pencil" aria-hidden="true"></i></button>
-                <button className="delete-btn btn" onClick={this.deleteTask.bind(this)}>Delete<i className="fa fa-trash" aria-hidden="true"></i></button>
+                <button className="btn btn-dark" onClick={this.deleteTask.bind(this)}>Delete<i className="fa fa-trash" aria-hidden="true"></i></button>
             </td>
         );
     }
